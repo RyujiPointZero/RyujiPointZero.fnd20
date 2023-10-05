47b7764d2f7f294685124eda0295e709272650e8
Ryuji-Inai <141981703+RyujiPointZero@users.noreply.github.com>
2023-10-05 15:22:22 +0900

    Update FinalDocRyuji.js

--- FinalDocRyuji.js
+++ FinalDocRyuji.js
@@ -2,12 +2,10 @@
 console.log("繋がったぜ!");
 function gators(){
     let element = document.getElementById('text');
-    // console.log(element.value);
-    for (let i = 0 ; i < element.value ; i++){
-        // console.log("i:",i)
-    document.getElementsByClassName("bluebox")[0].textContent += "🐊";
+        for (let i = 0 ; i < element.value ; i++){
+    document.getElementsByClassName("bluebox")[0].textContent += 1 + i;
     document.getElementsByClassName("bluebox")[0].style.backgroundColor = "red";
-}
+    }
 }
 
 const target = document.getElementsByClassName("button")[0];
